fd0cf523d4c0cbf6100b286562fe773ceae44c72
alumno <alumno@LAB00PC00>
2018-10-25 22:15:14 -0300

    Intento de TP3

---
+++ CASCARA_TP3/TP3/Linux_64/Controller.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+#include "parser.h"
+
+
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
+{
+    FILE* pArchivo = fopen(path,"r");
+    int retorno = -1;
+    retorno = parser_EmployeeFromText(pArchivo,pArrayListEmployee);
+    fclose(pArchivo);
+    return retorno;
+}
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee)
+{
+    int retorno = -1;
+    int i;
+    Employee* auxPunteroEmpleado;
+    char nombre[50];
+    int* horasTrabajadas = 0;
+    int sueldo = 0;
+    if(pArrayListEmployee!= NULL)
+    {
+        retorno = 0;
+        for(i=0;i<ll_len(pArrayListEmployee);i++)
+        {
+            auxPunteroEmpleado = ll_get(pArrayListEmployee,i);
+            Employee_getNombre(auxPunteroEmpleado,nombre);
+            Employee_getSueldo(auxPunteroEmpleado,sueldo);
+            printf("\nNombre: %s - Sueldo %d",nombre, &sueldo);
+        }
+    }
+    return retorno;
+}
+
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
